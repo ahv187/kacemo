37daf635675692d0b4a5c44ac64daff5f3a2790b
ahv187 <adrianohernanvillar@net-c.es>
2026-02-05 09:59:59 +0100

    fixes

--- api/events.js
+++ api/events.js
@@ -18,7 +18,7 @@ module.exports = async (req, res) => {
   try {
     if (req.method === 'GET') {
       let issues = await getIssues();
-      res.status(200).json(issues.unpack().data);
+      res.status(200).json(issues.unpack());
     } else if (req.method === 'POST') {
       // if(!authorizeApiCall(req, res)) { return; }
       const authHeader = req.headers.authorization;
@@ -40,8 +40,7 @@ module.exports = async (req, res) => {
       
       const { title, body } = req.body;
       let response = await pushIssue(title, body);
-      response.unpack();
-      res.status(201).json({ message: 'Event added successfully' });
+      res.status(201).json(response.unpack());
     } else {
       res.status(405).end(`Method ${req.method} Not Allowed`);
     }
